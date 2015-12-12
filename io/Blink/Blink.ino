2159541e639ac22d83e2b7b4e6a6f809fdd563c0
pincoin.info <pincoin.info@gmail.com>
2015-12-13 02:40:15 +0900

    Blink.ino LED 깜빡이기 에제

---
+++ io/Blink/Blink.ino
@@ -0,0 +1,13 @@
+// LED 깜빡이기 예제
+const int ledPin = 12;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+}
